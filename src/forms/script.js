51eddcb7e3da200ef32403c4d0994c5866aa347d
yanitaBiheza <yanita.biheza@student.ehu.lt>
2024-06-02 17:34:05 +0300

    feat: add solution forms

--- src/forms/script.js
+++ src/forms/script.js
@@ -1,1 +1,32 @@
-// Your solution here
+document.addEventListener('DOMContentLoaded', (event) => {
+    const form = document.querySelector('form');
+    const emailInput = document.getElementById('email');
+    const passwordInput = document.getElementById('password');
+    const repeatPasswordInput = document.getElementById('repeat-password');
+    const signUpButton = document.querySelector('.social-signup button');
+
+    signUpButton.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        let isFormValid = true;
+
+        const checkInput = (input) => {
+            if (input.value.trim() === '') {
+                input.style.borderColor = 'red';
+                isFormValid = false;
+            } else {
+                input.style.borderColor = ''; 
+            }
+        };
+
+        checkInput(emailInput);
+        checkInput(passwordInput);
+        checkInput(repeatPasswordInput);
+
+        if (isFormValid) {
+            alert('Success');
+        } else {
+            alert('Failure');
+        }
+    });
+});
